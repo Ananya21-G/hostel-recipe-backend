f78dfeaa817ceae4534089cc48a1b7f00c484e36
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-02-06 13:34:33 +0530

    Add recipe repository

--- src/main/java/com/hostel/recipe/repository/RecipeRepository.java
+++ src/main/java/com/hostel/recipe/repository/RecipeRepository.java
@@ -1,4 +1,11 @@
 package com.hostel.recipe.repository;
 
-public class RecipeRepository {
+import com.hostel.recipe.entity.Recipe;
+import com.hostel.recipe.entity.RecipeStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByStatus(RecipeStatus status);
 }
