16ab5fcc448680bd95294862450ef7c331406b81
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-02-07 12:13:47 +0530

    Add recipe response DTO

--- src/main/java/com/hostel/recipe/dto/RecipeResponseDTO.java
+++ src/main/java/com/hostel/recipe/dto/RecipeResponseDTO.java
@@ -2,4 +2,9 @@ package com.hostel.recipe.dto;
 
 public class RecipeResponseDTO {
 
-
+    private Long id;
+    private String title;
+    private String ingredients;
+    private String steps;
+    private Integer cookTime;
+}
