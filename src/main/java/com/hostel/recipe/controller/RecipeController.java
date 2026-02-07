b2f8557d08705a32aea82d32ce7f92a886593da8
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-02-07 11:44:16 +0530

    Add recipe controller

--- src/main/java/com/hostel/recipe/controller/RecipeController.java
+++ src/main/java/com/hostel/recipe/controller/RecipeController.java
@@ -1,4 +1,26 @@
 package com.hostel.recipe.controller;
 
+import com.hostel.recipe.entity.Recipe;
+import com.hostel.recipe.service.RecipeService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/recipes")
 public class RecipeController {
+    private final RecipeService recipeService;
+
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @PostMapping("/submit")
+    public Recipe submitRecipe(@RequestBody Recipe recipe) {
+        return recipeService.submitRecipe(recipe);
+    }
+    @GetMapping
+    public List<Recipe> getAllApprovedRecipe() {
+        return recipeService.getApprovedRecipe();
+    }
 }
