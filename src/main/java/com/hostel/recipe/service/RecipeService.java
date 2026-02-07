faaae40bfb2140211ece04c91837b855a4ef7853
Ananya21-G <ananyaghosh.2106@gmail.com>
2026-02-07 11:36:45 +0530

    Add recipe service layer

--- src/main/java/com/hostel/recipe/service/RecipeService.java
+++ src/main/java/com/hostel/recipe/service/RecipeService.java
@@ -1,4 +1,26 @@
 package com.hostel.recipe.service;
 
+import com.hostel.recipe.entity.Recipe;
+import com.hostel.recipe.entity.RecipeStatus;
+import com.hostel.recipe.repository.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
 public class RecipeService {
+    private final RecipeRepository recipeRepository;
+
+    public RecipeService(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    public Recipe submitRecipe(Recipe recipe) {
+        recipe.setStatus(RecipeStatus.PENDING);
+        return recipeRepository.save(recipe);
+    }
+    public list<Recipe> getApprovedRecipe() {
+        return recipeRepository.findByStatus(RecipeStatus.APPROVED);
+    }
 }
